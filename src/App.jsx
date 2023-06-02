d5e1d899e4ad01739466ac557bbff4dd9f408006
John Doe <crystinekoh@gmail.com>
2023-06-02 14:00:20 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Routes, Form } from "react-router-dom";
 import Signup from "./pages/signup";
 import Login from "./pages/login";
 import About from "./pages/about";
-import "./App.css";
 
 function App() {
   return <About></About>;
