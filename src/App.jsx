debf9d8748ba401cb5065e779af06869df3315f9
Indra Tinot-Patole <indra.tinot.patole@gmail.com>
2023-06-05 11:30:39 +0200

    fixing routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
 import PageList from "./Pages/PageList";
 import About from "./Pages/About";
-import FoundAPetForm from "./pages/FoundAPetForm";
+import FoundAPetForm from "./Pages/FoundAPetForm";
 
 // IMPORT COMPONENTS
 import Footer from "./Components/Footer";
@@ -17,42 +17,43 @@ import Footer from "./Components/Footer";
 // import Sort from "./Components/Sort";
 
 function App() {
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setError("");
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setLoading(true);
+		setError("");
 
-    try {
-      const response = await axios.post("/auth/login", {
-        email,
-        password,
-      });
-      const { authToken } = response.data;
+		try {
+			const response = await axios.post("/auth/login", {
+				email,
+				password,
+			});
+			const { authToken } = response.data;
 
-      localStorage.setItem("authToken", authToken);
-      setLoading(false);
-      setError("");
-    } catch (error) {
-      setLoading(false);
-      setError(error.message);
-    }
-  };
-  return (
-    <>
-      <Routes>
-        <Route element={<Footer />}>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route
-            path="/login"
-            element={<Login handleSubmit={handleSubmit} />}
-          />
-          <Route path="/pets" element={<PageList />} />
-          <Route path="/About" element={<About />} />
-        </Route>
-      </Routes>
-    </>
-  );
+			localStorage.setItem("authToken", authToken);
+			setLoading(false);
+			setError("");
+		} catch (error) {
+			setLoading(false);
+			setError(error.message);
+		}
+	};
+	return (
+		<>
+			<Routes>
+				<Route element={<Footer />}>
+					<Route path="/" element={<LandingPage />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route
+						path="/login"
+						element={<Login handleSubmit={handleSubmit} />}
+					/>
+					<Route path="/pets" element={<PageList />} />
+					<Route path="/found-a-pet" element={<FoundAPetForm />} />
+					<Route path="/About" element={<About />} />
+				</Route>
+			</Routes>
+		</>
+	);
 }
 
 export default App;
