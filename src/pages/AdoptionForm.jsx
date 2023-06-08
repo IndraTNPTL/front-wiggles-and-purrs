937f3c8dbef63b9cf5dbbb64b99e661e393767bc
Indra Tinot-Patole <indra.tinot.patole@gmail.com>
2023-06-08 18:08:56 +0200

    deploying

--- src/pages/AdoptionForm.jsx
+++ src/pages/AdoptionForm.jsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 import service from "../../service/api";
 
 // import { Link } from "react-router-dom";
-import { AuthContext } from "../../service/AuthContext";
+import { AuthContext } from "../../service/authContext";
 
 // import headerPic from "../assets/adoptPet.jpeg";
 
